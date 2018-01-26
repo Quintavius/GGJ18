3c8977d8efaa313854156486ca25b31e96b7575f
Quintavius <loths.marc@gmail.com>
2018-01-27 12:27:54 +1300

    Testing out how terrain would look

---
+++ GGJ/Assets/Scripts/PlaceholderControls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceholderControls : MonoBehaviour {
+	CharacterController character;
+	public float speed = 5;
+	public float jump = 6;
+	// Use this for initialization
+	void Start () {
+		character = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		character.SimpleMove(new Vector3 (0,-3,0));
+
+		if (character.isGrounded){
+			Vector3 move = new Vector3(Input.GetAxis("Horizontal") * 5,Input.GetAxis("Vertical") * jump,0);
+			character.SimpleMove(move);
+		}
+	}
+}
