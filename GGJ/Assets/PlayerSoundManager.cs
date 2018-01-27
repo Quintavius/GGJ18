c8ee8a24bf6b18c079fe63deea04dc8d5359b333
Quintavius <loths.marc@gmail.com>
2018-01-28 10:24:40 +1300

    Added Jump & Drop Sprites

---
+++ GGJ/Assets/PlayerSoundManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSoundManager : MonoBehaviour {
+	[Header("SourceSounds")]
+	public AudioClip[] bubblesSteps;
+	public AudioClip[] puddlesSteps;
+	public AudioClip[] jumpSounds;
+	public AudioClip[] dropSounds;
+	public AudioClip[] pushSounds;
+
+	//References
+	Animator ani;
+	player2 movement;
+
+	private void Start(){
+		movement = FindObjectOfType<player2>();
+		ani = GetComponentInChildren<Animator>();
+	}
+	
+	private void Update(){
+		
+	}
+
+}
