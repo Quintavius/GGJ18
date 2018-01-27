2690a7c158e89421fa44fb263f4eda21459647d4
Quintavius <loths.marc@gmail.com>
2018-01-28 12:43:54 +1300

    Idle animations

---
+++ GGJ/Assets/PhysicsObjectSoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObjectSoundManager : MonoBehaviour {
+	public AudioClip[] impactSounds;
+	AudioSource source;
+	Rigidbody rb;
+
+	private void Start(){
+		source = GetComponent<AudioSource>();
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void OnCollisionEnter(Collision col){
+		var volumeMod = Mathf.Clamp01(rb.velocity.magnitude / 5);
+		source.volume = volumeMod;
+		int soundArraySize = impactSounds.Length;
+		AudioClip chosenImpactSound = impactSounds[Random.Range(0,soundArraySize)];
+		source.PlayOneShot(chosenImpactSound);
+	}
+}
