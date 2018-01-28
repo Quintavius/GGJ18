9a5962df082c651348eb659651f4f7efbb2b22b5
Quintavius <loths.marc@gmail.com>
2018-01-28 14:21:41 +1300

    Push before merge

--- GGJ/Assets/Scripts/PlaceholderControls.cs
+++ GGJ/Assets/Scripts/PlaceholderControls.cs
@@ -4,21 +4,47 @@ using UnityEngine;
 
 public class PlaceholderControls : MonoBehaviour {
 	CharacterController character;
+	public Rigidbody pushCube;
 	public float speed = 5;
 	public float jump = 6;
+	public float gravity = 9;
+	LightState lit;
 	// Use this for initialization
 	void Start () {
 		character = GetComponent<CharacterController>();
+		lit = GetComponent<LightState>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		character.SimpleMove(new Vector3 (0,-3,0));
+		transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+
+		character.SimpleMove(new Vector3 (0,-gravity,0));
 
 		if (character.isGrounded){
 			Vector3 move = new Vector3(Input.GetAxis("Horizontal") * speed, 0,0);
 			character.SimpleMove(move);
-			character.Move(new Vector3(0, System.Convert.ToSingle(Input.GetButtonDown("Jump")) * jump, 0));
+			float litState = 1 - System.Convert.ToSingle(lit.playerIsLit);
+			character.Move(new Vector3(0, litState * System.Convert.ToSingle(Input.GetButtonDown("Jump")) * jump * Time.deltaTime, 0));
+		}	
+	}
+
+	private void OnCollisionStay(Collision other){
+		if (other.gameObject.tag == "Moveable"){
+			if (!lit.playerIsLit){
+				other.gameObject.GetComponent<Rigidbody>().constraints =
+					RigidbodyConstraints.FreezePositionZ |
+					RigidbodyConstraints.FreezeRotationX |
+					RigidbodyConstraints.FreezeRotationY ;
+			}else{
+				other.gameObject.GetComponent<Rigidbody>().constraints =
+					RigidbodyConstraints.FreezePositionX |
+					RigidbodyConstraints.FreezePositionY |
+					RigidbodyConstraints.FreezePositionZ |
+					RigidbodyConstraints.FreezeRotationX |
+					RigidbodyConstraints.FreezeRotationY |
+					RigidbodyConstraints.FreezeRotationZ ;
+			}
 		}
 	}
 }
